396407ff85e8017e923dc4cfef3ce7b54b2ec411
Feroz khan <ferozkhan27@gmail.com>
2020-05-04 01:10:46 +0200

    add find sum

---
+++ vigilant-winner/code/movie_combination.py
@@ -0,0 +1,53 @@
+import unittest
+from typing import List
+
+
+def can_two_movies_fill_flight(movies_length: List[int], flight_length: int) -> bool:
+    # to store movie length seen so far
+    movie_length_seen = set()
+    for movie_length in movies_length:
+        if movie_length in movie_length_seen:
+            return True
+        movie_length_seen.add(flight_length - movie_length)
+
+    # no two movies found matches flight length, return assertFalse
+    return False
+
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_not_using_first_movie(self):
+        result = can_two_movies_fill_flight([4, 3, 2], 5)
+        self.assertTrue(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
