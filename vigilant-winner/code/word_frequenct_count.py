6454670598476017a6f64a22d69693732f2a0321
Feroz khan <ferozkhan27@gmail.com>
2020-05-10 20:32:23 +0200

    add word freq count

---
+++ vigilant-winner/code/word_frequenct_count.py
@@ -0,0 +1,42 @@
+import operator
+import string
+import sys
+from collections import defaultdict
+
+
+def lines_from_file(filename):
+    """ a generator yielding lines from filename """
+    with open(filename) as file_handler:
+        for _line in file_handler:
+            yield _line.strip('\n')
+
+
+def words_from_line(lines):
+    """ a generator yielding word from lines """
+    for _line in lines:
+        for _word in _line.split(' '):
+            if _word not in string.whitespace:
+                yield _word
+
+
+def get_words_count(filename):
+    word_count = defaultdict(int)
+    lines = lines_from_file(filename)
+    words = words_from_line(lines)
+    for word in words:
+        word_count[word] += 1
+    return word_count
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("error: filename is required")
+        print("-> word_count.py <filename> <top-N-word>")
+        sys.exit()
+    top_n_words = int(sys.argv[2]) if len(sys.argv) > 2 else None
+    word_count = list(sorted(get_words_count(sys.argv[1]).items(),
+                        key=operator.itemgetter(1), reverse=True))
+    if top_n_words:
+        print(word_count[:top_n_words], end='\n')
+    else:
+        print(word_count, end='\n')
